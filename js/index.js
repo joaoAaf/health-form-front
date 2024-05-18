70b354b40020342a967ee3ccae5ea5ff6f2e5e0c
joaoAaf <joao.anderson.af@gmail.com>
2024-05-18 12:50:21 -0300

    corrigindo chamada para ao service-login

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const loginUrl = "http://localhost:8765/login"
+const loginForm = document.getElementById("loginForm")
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  login();
+});
+
+async function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  const loginData = {
+    "email": username,
+    "pass": password
+  };
+
+  try {
+    const response = await fetch(loginUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginData),
+    });
+
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`Falha ao fazer login: ${responseJson.msg}`);
+    }
+
+    console.log(responseJson.msg);
+    sessionStorage.setItem("token", responseJson.data);
+
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  }
+}
